f65d01be62a046916547714250cdab916724a616
Jon Surrell <jon.surrell@automattic.com>
2019-03-05 08:23:35 -0300

    remove class names
    Co-Authored-By: lezama <lezama@gmail.com>

--- client/gutenberg/extensions/presets/jetpack/utils/jetpack-logo.jsx
+++ client/gutenberg/extensions/presets/jetpack/utils/jetpack-logo.jsx
@@ -14,7 +14,7 @@ const JetpackLogo = () => (
 			d="M16,0C7.2,0,0,7.2,0,16s7.2,16,16,16s16-7.2,16-16S24.8,0,16,0z"
 		/>
 		<Polygon fill="#fff" points="15,19 7,19 15,3 " />
-		<Polygon className="jetpack-logo__icon-triangle" fill="#fff" points="17,29 17,13 25,13 " />
+		<Polygon fill="#fff" points="17,29 17,13 25,13 " />
 	</SVG>
 );
 
